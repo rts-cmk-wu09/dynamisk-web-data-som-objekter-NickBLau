e2372b6d989d11c9dafe843b42559996a17876fa
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-02-07 11:26:32 +0100

    add opgave 2

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
+// let person = {
+//     firstName: "Jim",
+//     lastName: "Peterson",
+//     job: "Taxi-driver",
+//     favoriteFood: "Pizza",
+//     transport: "Bike",
+// }
+
+// console.log("opgave 1");
+// console.log(person.transport);
+// console.log(person["favoriteFood"]);
+
+
 let person = {
     firstName: "Jim",
     lastName: "Peterson",
     job: "Taxi-driver",
     favoriteFood: "Pizza",
     transport: "Bike",
+    driversLicence: false,
+    pets: ["Cat", "Bird", "Fish", "Hamster"],
+    family: {
+        type: "Sibling",
+        firstName: "Alice",
+        lastName: "Peterson"
+    }
+
 }
 
-console.log("opgave 1");
-console.log(person.transport);
-console.log(person["favoriteFood"]);
-
+console.log("opgave 2");
+console.log(person.driversLicence);
+person.pets.forEach(function (pets){console.log(pets)
+});
+console.log(person.family.firstName);
