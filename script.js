98a64dd26698d04dbc6f9aef3b71804afdf5ebb2
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-02-07 11:10:16 +0100

    add opgave 1

---
+++ script.js
@@ -0,0 +1,12 @@
+let person = {
+    firstName: "Jim",
+    lastName: "Peterson",
+    job: "Taxi-driver",
+    favoriteFood: "Pizza",
+    transport: "Bike",
+}
+
+console.log("opgave 1");
+console.log(person.transport);
+console.log(person["favoriteFood"]);
+
