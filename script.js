92d97327b01358933f0fd03a1a57a86076f1b312
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-02-08 10:43:17 +0100

    add minListe objects and arrays

--- script.js
+++ script.js
@@ -11,48 +11,71 @@
 // console.log(person["favoriteFood"]);
 
 
-let person = {
-    firstName: "Jim",
-    lastName: "Peterson",
-    job: "Taxi-driver",
-    favoriteFood: "Pizza",
-    transport: "Bike",
-    driversLicence: false,
-    pets: ["Cat", "Bird", "Fish", "Hamster"],
-    family: {
-        type: "Sibling",
-        firstName: "Alice",
-        lastName: "Peterson"
+// let person = {
+//     firstName: "Jim",
+//     lastName: "Peterson",
+//     job: "Taxi-driver",
+//     favoriteFood: "Pizza",
+//     transport: "Bike",
+//     driversLicence: false,
+//     pets: ["Cat", "Bird", "Fish", "Hamster"],
+//     family: {
+//         type: "Sibling",
+//         firstName: "Alice",
+//         lastName: "Peterson"
+//     }
+
+// }
+
+// console.log("opgave 2");
+
+// console.log(" ")
+
+// console.log(person.driversLicence);
+
+// console.log(" ")
+
+// person.pets.forEach(function (pets){console.log(pets)
+// });
+
+// console.log(" ")
+
+// console.log(person.family.type);
+// console.log(person.family.firstName);
+// console.log(person.family.lastName);
+
+// console.log(" ")
+
+
+// for (let i=0; i<[person.family].length; i++){
+//     console.log(person.family);
+// }
+
+// console.log(" ")
+
+// for (let i=0; i<[person.pets].length; i++){
+//     console.log(person.pets);
+// }
+
+
+let minListe = { 
+film:{},
+tv:{},
+Spil:{
+ rogueLite:"Hades",[
+    {
+        dato: "september, 2020",
+        score: "10/10",
+        pris: "24.50£",
+        publisher: "Supergiant games"
     }
-
+ ],
+ 
+ sandbox: ["Minecraft", "Terraria"],
+ fps: ["Doom", "superhot", "Counter-strike"],
+ deckBuilder: "Slay the spire",
+ Survivor: ["Vampire survivor", "holocure"]
+}
 }
 
-console.log("opgave 2");
-
-console.log(" ")
-
-console.log(person.driversLicence);
-
-console.log(" ")
-
-person.pets.forEach(function (pets){console.log(pets)
-});
-
-console.log(" ")
-
-console.log(person.family.type);
-console.log(person.family.firstName);
-console.log(person.family.lastName);
-
-console.log(" ")
-
-
-for (let i=0; i<[person.family].length; i++){
-    console.log(person.family);
-}
-
-console.log(" ")
-
-for (let i=0; i<[person.pets].length; i++){
-    console.log(person.pets);
-}
+console.log(spil.fps);
