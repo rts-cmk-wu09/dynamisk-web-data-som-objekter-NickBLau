2a595bbd505b789a49090242909daaebb7146390
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-02-09 13:02:23 +0100

    add change from sandbox to relevant info

--- myindex.js
+++ myindex.js
@@ -3,11 +3,33 @@ document.querySelector("#data").innerHTML =
 <h1> Min liste af ting
 
 <h2> sidst spillede genre: ${minListe.spil} </h2>
-<p> rogutelites: ${minListe.rogueLites.hades.title} , ${minListe.rogueLites.andet.title}  </p>
+<p> rogutelites: ${minListe.rogueLites.hades.title} , ${minListe.rogueLites.rogueLegacy.title}  </p>
 
+<p class="red"> heelllooo</p>
 
 <h3> roguelite: ${minListe.rogueLites.hades.title} </h3>
+<p> dato: ${minListe.rogueLites.hades.dato} </p>
+<p> score: ${minListe.rogueLites.hades.score} </p>
+<p> pris: ${minListe.rogueLites.hades.pris} </p>
+<p> publisher: ${minListe.rogueLites.hades.publisher} </p>
+<p> platform: ${minListe.rogueLites.hades.platform} </p>
+
+<h3> roguelite: ${minListe.rogueLites.rogueLegacy.title} </h3>
+<p> dato: ${minListe.rogueLites.rogueLegacy.dato} </p>
+<p> score: ${minListe.rogueLites.rogueLegacy.score} </p>
+<p> pris: ${minListe.rogueLites.rogueLegacy.pris} </p>
+<p> publisher: ${minListe.rogueLites.rogueLegacy.publisher} </p>
+<p> platform: ${minListe.rogueLites.rogueLegacy.platform} </p>
+
+
+<h3> sandbox: ${minListe.sandbox.minecraft.title} </h3>
 <p> sandbox: ${minListe.sandbox.minecraft.dato} </p>
+<p> sandbox: ${minListe.sandbox.minecraft.score} </p>
+<p> sandbox: ${minListe.sandbox.minecraft.pris} </p>
+<p> sandbox: ${minListe.sandbox.minecraft.publisher} </p>
+<p> sandbox: ${minListe.sandbox.minecraft.platform} </p>
+
+<p> sandbox: ${minListe.sandbox.minecraft} </p>
 
 <h2> sidst set tv: ${minListe.tv} </h2>
 </section>`
